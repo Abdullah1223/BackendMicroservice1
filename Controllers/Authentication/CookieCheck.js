9c3fa02c4e1755904efa6a760abf065ed9710d70
Abdullah1223 <72107599+Abdullah1223@users.noreply.github.com>
2025-02-07 05:41:38 +0500

    Update CookieCheck.js

--- Controllers/Authentication/CookieCheck.js
+++ Controllers/Authentication/CookieCheck.js
@@ -10,7 +10,8 @@ const CookieCheck = (req,res)=>{
             res.clearCookie('token', {
                 httpOnly: true, // Ensures it can only be accessed by the server
                 secure: true, // Ensures it's only sent over HTTPS in production
-                sameSite: 'Strict', // Restrict the cookie to same-site requests
+                //sameSite: 'Strict',
+                 sameSite:'None'                    // Restrict the cookie to same-site requests
             });
             return res.status(400).send({message:'Token is Tampered'})
             
